56337a50452877036b1c965bf79b1dda37ba66b3
elle7777 <iustindesrobitu@gmail.com>
2024-09-03 10:52:03 +0100

    a

--- Exercises/Basic Programms/Python program to check armstrong number/main.py
+++ Exercises/Basic Programms/Python program to check armstrong number/main.py
@@ -2,10 +2,27 @@
 #version 0.1.0
 #@Iustin Desrobitu
 
-def armstrong_number_check(user_number_in):
-    print(user_number_in[0])
+def index_separator(user_number_in, index):
+    number_at_index = (user_number_in % 10 ** index) / 10 ** (index - 1)
+    return(int(number_at_index))
 
-print("This program take a number and check if it's an armstrong number")
-user_number = int(input("insert a number: "))
+def armstrong_number_check (user_number_in):
+    index = len(str(user_number_in))
+    user_number_length = len(str(user_number_in))
+    armstrong_number = 0
+    while index > 0:
+        armstrong_number = armstrong_number + index_separator(user_number_in, index) ** user_number_length
+        index = index - 1
+    if armstrong_number == user_number_in:
+        return True
+    else:
+        return False
 
-armstrong_number_check(user_number)
+while True:
+    print("This program take a number and check if it's an armstrong number")
+    user_number = int(input("insert a number: "))
+
+    if armstrong_number_check(user_number): 
+        print(f"{user_number} is an armstrong number")
+    else:
+        print(f"{user_number} is not an armstrong number")
